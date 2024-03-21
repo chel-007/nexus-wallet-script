af6ad61a3910f673a9053e84734e87d3c0eb581b
Chel Koby <58524269+chel-007@users.noreply.github.com>
2024-03-21 06:10:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 3001 || process.env.PORT;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 
